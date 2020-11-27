4e2856c0e8477f30c1b82f8ecab4c8f6ddc093ac
xuwei <email.xuwei@gmail.com>
2020-11-27 11:03:05 +1100

    initial commit

---
+++ ImageURLLoader/ImageURLLoader.h
@@ -0,0 +1,18 @@
+//
+//  ImageURLLoader.h
+//  ImageURLLoader
+//
+//  Created by Xuwei Liang on 27/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageURLLoader.
+FOUNDATION_EXPORT double ImageURLLoaderVersionNumber;
+
+//! Project version string for ImageURLLoader.
+FOUNDATION_EXPORT const unsigned char ImageURLLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageURLLoader/PublicHeader.h>
+
+
---
+++ ImageURLLoaderExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageURLLoaderExamples
+//
+//  Created by Xuwei Liang on 27/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
